393774d7c76e17cc94c0d3a18211c86e5626d7b5
djd4532 <djd453216@nate.com>
2021-03-23 09:21:31 +0900

    210322 MicroService Lecture

--- be/demo/src/main/java/com/example/demo/uss/domain/User.java
+++ be/demo/src/main/java/com/example/demo/uss/domain/User.java
@@ -13,16 +13,25 @@ public class User {
 	@Column(name = "user_No")
 	private long userNo;
 	
-	@Column(name = "userid")
-	private String userid;
+	@Column(name = "name")
+	private String name;
 	
-	@Column(name = "passwd")
-	private String passwd;
+	@Column(name = "password")
+	private String password;
 	
 	@Column(name = "username")
 	private String username;
 	
 	@Column(name = "email")
 	private String email;
+	
+	@Column(name = "birthday")
+	private String birthday;
+	
+	@Column(name = "gender")
+	private String gender;
+	
+	@Column(name = "regdate")
+	private String regdate;
 
 }
